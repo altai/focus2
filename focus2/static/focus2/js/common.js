d9b2f2e4b4d05ffea2d26898e029d03d412f8f67
Alessio Ababilov <aababilov@griddynamics.com>
2013-03-12 16:29:56 +0200

    Enable daterange and tooltips in common.js
    Change-Id: I10651528933750dddc9d90b2c7cc7cc821b21a3d

--- focus2/static/focus2/js/common.js
+++ focus2/static/focus2/js/common.js
@@ -59,6 +59,18 @@ jQuery(function($) {
         options.allowClear = (typeof($this.attr("allow-clear")) != undefined);
         $this.select2(options);
     });
+    $(".help-tooltip").tooltip();
+    $("[rel=tooltip]").tooltip();
+    $(".daterange").daterangepicker({
+        ranges: {
+            "Today": ["today", "today"],
+            "Yesterday": ["yesterday", "yesterday"],
+            "Last 7 Days": [Date.today().add({days: -6}), "today"],
+            "Last 30 Days": [Date.today().add({days: -29}), "today"],
+            "This Month": [Date.today().moveToFirstDayOfMonth(), Date.today().moveToLastDayOfMonth()],
+            "Last Month": [Date.today().moveToFirstDayOfMonth().add({months: -1}), Date.today().moveToFirstDayOfMonth().add({days: -1})]
+        }
+    });
 });
 
 
